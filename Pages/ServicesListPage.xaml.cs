46ea7afa69cefb8b303c0e72f8ebeb0618b7f62d
Mox1k <prizrachina@gmail.com>
2022-11-24 12:48:29 +0300

    Добавил новую страницу

--- Pages/ServicesListPage.xaml.cs
+++ Pages/ServicesListPage.xaml.cs
@@ -13,28 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BeautyServicee
+namespace BeautyServicee.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ServicesListPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ServicesListPage : Page
     {
-        public MainWindow()
+        public ServicesListPage()
         {
             InitializeComponent();
-        }
-
-        private void AddService(object sender, RoutedEventArgs e)
-        {
-            MainFrame.NavigationService.Navigate(new Pages.AddServicePage());
-
-        }
-
-        private void ServicesList(object sender, RoutedEventArgs e)
-        {
-            MainFrame.NavigationService.Navigate(new Pages.ServicesListPage());
-
         }
     }
 }
